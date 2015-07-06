bad56bf2a60cffbb79e97daa02252aa4dcf9b278
Justin Huntington <corusco@gmail.com>
2015-07-06 10:41:23 -0600

    Adds Visual Flair and Tweaks UI.

---
+++ ReminderCapstone/PhotoDetailNavController.h
@@ -0,0 +1,13 @@
+//
+//  PhotoDetailNavController.h
+//  ReminderCapstone
+//
+//  Created by Justin Huntington on 6/29/15.
+//  Copyright (c) 2015 CVLCD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoDetailNavController : UINavigationController
+
+@end
