66558065bde1fdc1d81524da0cef224d7af93374
Justin Huntington <corusco@gmail.com>
2015-10-07 09:20:19 -0600

    Stage before fixing disappearing photos.

---
+++ ReminderCapstone/ThemeQueryController.h
@@ -0,0 +1,13 @@
+//
+//  ThemeQueryController.h
+//  ReminderCapstone
+//
+//  Created by Justin Huntington on 9/30/15.
+//  Copyright (c) 2015 CVLCD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ThemeQueryController : NSObject
+
+@end
---
+++ ReminderCapstone/ThemeQueryController.m
@@ -0,0 +1,13 @@
+//
+//  ThemeQueryController.m
+//  ReminderCapstone
+//
+//  Created by Justin Huntington on 9/30/15.
+//  Copyright (c) 2015 CVLCD. All rights reserved.
+//
+
+#import "ThemeQueryController.h"
+
+@implementation ThemeQueryController
+
+@end
