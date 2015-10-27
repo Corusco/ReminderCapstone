07d30bd0491c8b604f2d15c5dd6623440d383240
Justin Huntington <corusco@gmail.com>
2015-10-27 09:59:17 -0600

    Pulls friendFeed child and segmented control. Runs

--- ReminderCapstone/MainFeedViewController.m
+++ ReminderCapstone/MainFeedViewController.m
@@ -50,35 +50,11 @@
     [self.introLabel constrainHeight:@"20"];
     [self.introLabel alignLeadingEdgeWithView:self.headerTheme predicate:@"0"];
     
-    self.segmentView = [[UIView alloc] init];
-    self.segmentView.backgroundColor = UIColorFromRGB(0x7FADAD);
-    self.segmentView.tintColor = [UIColor whiteColor];
-    [self.view addSubview:self.segmentView];
-    [self.segmentView constrainTopSpaceToView:self.headerView predicate:@"0"];
-    [self.segmentView alignLeading:@"0" trailing:@"0" toView:self.view];
-    [self.segmentView constrainHeight:@"44"];
-    
-    self.segmentedControl = [[UISegmentedControl alloc] initWithItems:@[@"From Your Friends", @"Global Inspiration"]];
-    self.segmentedControl.selectedSegmentIndex = 1;
-    [self.segmentView addSubview:self.segmentedControl];
-    [self.segmentedControl alignCenterWithView:self.segmentView];
-    [self.segmentedControl addTarget:self
-                              action:@selector(segmentedControlChanged:)
-                    forControlEvents:UIControlEventValueChanged];
-    
-    self.friendFeedChild = [[FriendFeedChildViewController alloc] init];
-    [self addChildViewController:self.friendFeedChild];
-    [self.view addSubview:self.friendFeedChild.view];
-    self.friendFeedChild.view.hidden = NO;
-    [self.friendFeedChild.view constrainTopSpaceToView:self.segmentView predicate:@"0"];
-    [self.friendFeedChild.view alignLeading:@"0" trailing:@"0" toView:self.view];
-    [self.friendFeedChild.view alignBottomEdgeWithView:self.view predicate:@"0"];
-    
     self.globalFeedChild = [[GlobalFeedChildViewController alloc] init];
     [self addChildViewController:self.globalFeedChild];
     self.globalFeedChild.view.hidden = NO;
     [self.view addSubview:self.globalFeedChild.view];
-    [self.globalFeedChild.view constrainTopSpaceToView:self.segmentView predicate:@"0"];
+    [self.globalFeedChild.view constrainTopSpaceToView:self.headerView predicate:@"0"];
     [self.globalFeedChild.view alignLeading:@"0" trailing:@"0" toView:self.view];
     [self.globalFeedChild.view alignBottomEdgeWithView:self.view predicate:@"0"];
     
@@ -86,86 +62,5 @@
 }
 
 
-- (void)showFriendFeedChild {
-    if (self.currentChild == self.friendFeedChild) {
-        return;
-    } else {
-        [self addChildViewController:self.friendFeedChild];
-        
-        [self transitionFromViewController:self.currentChild
-                          toViewController:self.friendFeedChild
-                                  duration:0.5
-                                   options:UIViewAnimationOptionTransitionNone
-                                animations:nil
-                                completion:^(BOOL finished) {
-                                    [self.globalFeedChild willMoveToParentViewController:nil];
-                                    [self.currentChild willMoveToParentViewController:nil];
-                                    [self.currentChild.view removeFromSuperview];
-                                    [self.globalFeedChild.view removeFromSuperview];
-                                    [self.currentChild removeFromParentViewController];
-                                    [self.globalFeedChild removeFromParentViewController];
-                                    [self.friendFeedChild didMoveToParentViewController:self];
-                                    self.globalFeedChild.view.hidden = YES;
-                                    self.friendFeedChild.view.hidden = NO;
-                                    [self.friendFeedChild.view constrainTopSpaceToView:self.segmentView predicate:@"0"];
-                                    [self.friendFeedChild.view alignLeading:@"0" trailing:@"0" toView:self.view];
-                                    [self.friendFeedChild.view alignBottomEdgeWithView:self.view predicate:@"0"];
-                                    self.currentChild = self.friendFeedChild;
-                                    
-                                    [self.friendFeedChild.collectionView alignBottomEdgeWithView:self.friendFeedChild.view predicate:@"0"];
-                                    [self.friendFeedChild.collectionView constrainHeightToView:self.friendFeedChild.view predicate:@"0"];
-                                    [self.friendFeedChild.collectionView constrainWidthToView:self.friendFeedChild.view predicate:@"0"];
-                                    
-                                    NSLog(@"Friend Child Finished");
-                                }];
-    }
-}
-
-
-- (void)showGlobalFeedChild {
-    if (self.currentChild == self.globalFeedChild) {
-        return;
-    } else {
-        [self addChildViewController:self.globalFeedChild];
-        
-        [self transitionFromViewController:self.currentChild
-                          toViewController:self.globalFeedChild
-                                  duration:0.5
-                                   options:UIViewAnimationOptionTransitionNone
-                                animations:nil
-                                completion:^(BOOL finished) {
-                                    [self.friendFeedChild willMoveToParentViewController:nil];
-                                    [self.currentChild willMoveToParentViewController:nil];
-                                    [self.currentChild.view removeFromSuperview];
-                                    [self.friendFeedChild.view removeFromSuperview];
-                                    [self.currentChild removeFromParentViewController];
-                                    [self.friendFeedChild removeFromParentViewController];
-                                    [self.globalFeedChild didMoveToParentViewController:self];
-                                    self.friendFeedChild.view.hidden = YES;
-                                    self.globalFeedChild.view.hidden = NO;
-                                    [self.globalFeedChild.view constrainTopSpaceToView:self.segmentView predicate:@"0"];
-                                    [self.globalFeedChild.view alignLeading:@"0" trailing:@"0" toView:self.view];
-                                    [self.globalFeedChild.view alignBottomEdgeWithView:self.view predicate:@"0"];
-                                    self.currentChild = self.globalFeedChild;
-                                    
-                                    [self.globalFeedChild.collectionView alignBottomEdgeWithView:self.globalFeedChild.view predicate:@"0"];
-                                    [self.globalFeedChild.collectionView constrainHeightToView:self.globalFeedChild.view predicate:@"0"];
-                                    [self.globalFeedChild.collectionView constrainWidthToView:self.globalFeedChild.view predicate:@"0"];
-                                    
-                                    NSLog(@"Global Child Finished");
-                                }];
-    }
-}
-
-- (void)segmentedControlChanged:(UISegmentedControl *)paramSender {
-    if ([paramSender isEqual: self.segmentedControl]) {
-        
-        if (self.segmentedControl.selectedSegmentIndex == 0) {
-            [self showFriendFeedChild];
-        } else if (self.segmentedControl.selectedSegmentIndex == 1) {
-            [self showGlobalFeedChild];
-        }
-    }
-}
 
 @end
